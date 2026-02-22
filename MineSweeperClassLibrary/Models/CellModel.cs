10b35a31fc577df8dfd98af460b947f6e8b61ab3
Jacob Cutler <cutlerjacob18@gmail.com>
2026-02-22 12:05:38 -0700

    Add files via upload

---
+++ MineSweeperClassLibrary/Models/CellModel.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Jacob Cutler
+ * CST-250
+ * 02/22/2026
+ * Mine Sweeper Game
+ * Milestone Project
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeperClassLibrary.Models
+{
+    public class CellModel
+    {
+        // Class level properties
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+        public bool IsMine { get; set; }
+        public bool IsRevealed { get; set; }
+        public bool IsFlagged { get; set; }
+        public bool IsReward { get; set; }
+        public bool IsDiffused { get; set; }
+        public bool IsFilled { get; set; }
+
+        /// <summary>
+        /// Default constructor for CellModel
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        public CellModel(int row, int column)
+        {
+            // Initialize the properties for the model
+            Row = row;
+            Column = column;
+
+            // Set the default value of the properties
+            IsMine = false;
+            IsRevealed = false;
+            IsFlagged = false;
+            IsReward = false;
+            IsDiffused = false;
+            IsFilled = false;
+        }
+    }
+}
